7117b10a7a5bdd50c4595621b0f27ee04da1f79b
Rohit Jaiswal <rj810419@gmail.com>
2024-06-20 11:26:08 +0530

    Adding the new file and modify the privious file

---
+++ New.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a 1st numbre: "))
+b = int(input("Enter a 2nd number: "))
+c=a+b
+if c%2==0:
+    print("This is a Even Number: ",c)
+else:
+    print("This is a Prime Number: ",c)    
